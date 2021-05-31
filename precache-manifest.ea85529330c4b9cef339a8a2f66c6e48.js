a26e41f9e79f3552ce4e57d6062adf77f52752a5
Brigham Andersen <brighamband@gmail.com>
2021-05-31 15:37:07 -0600

    Updates

---
+++ precache-manifest.ea85529330c4b9cef339a8a2f66c6e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7822381b0000e1c786dc9585a82e795d",
+    "url": "/simple-counter/index.html"
+  },
+  {
+    "revision": "249cfb468852987f6c09",
+    "url": "/simple-counter/static/css/main.bc11a4e6.chunk.css"
+  },
+  {
+    "revision": "d6670a0fd120cedd7b9a",
+    "url": "/simple-counter/static/js/2.8426f74b.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/simple-counter/static/js/2.8426f74b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249cfb468852987f6c09",
+    "url": "/simple-counter/static/js/main.a606ebdd.chunk.js"
+  },
+  {
+    "revision": "01139460c8afb21d1bf9",
+    "url": "/simple-counter/static/js/runtime-main.5bd42668.js"
+  }
+]);
